f4c55b8877211221ccb52022d1e932cf036d8d61
Brigit Danthine <97695048+brigitdanthine@users.noreply.github.com>
2022-04-05 21:20:27 +0200

    Add files via upload

---
+++ resources.bat
@@ -0,0 +1,6 @@
+@echo off
+call "C:\Program Files\QGIS 3.4\bin\o4w_env.bat"
+call "C:\Program Files\QGIS 3.4\bin\qt5_env.bat"
+call "C:\Program Files\QGIS 3.4\bin\py3_env.bat"
+
+pyrcc5 -o resources.py resources.qrc
